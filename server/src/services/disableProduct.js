e3c22dd84b902d3f0b95e88a19752c4fd89b04f9
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-04 11:07:41 +0700

    fix: edit indent in disableProduct

--- server/src/services/disableProduct.js
+++ server/src/services/disableProduct.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 
 const disableProduct = async (product_id, detail, status) => {
   return new Promise((resolve, reject) => {
-    const SQL = `UPDATE product SET 
-                    status = (SELECT id FROM product_status WHERE status_value = ${status}),
-                    detail = ${mysql.escape(detail)}
-                    WHERE product_id = ${mysql.escape(product_id)};`;
+    const SQL = `UPDATE product 
+                  SET status = (SELECT id FROM product_status WHERE status_value = ${status}),
+                      detail = ${mysql.escape(detail)}
+                  WHERE product_id = ${mysql.escape(product_id)};`;
     connection.query(SQL, (error, result, field) => {
       if (error) return reject(error);
       resolve(result);
