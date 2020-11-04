4fbba937b2a3752cf59d20a924f8ef3e427eaa8c
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-04 11:19:50 +0700

    fix: edit indent in getRole

--- server/src/services/getRole.js
+++ server/src/services/getRole.js
@@ -7,16 +7,16 @@ const getRole = async (
 ) => {
   return new Promise((resolve, reject) => {
     let SQL = `SELECT 
-                  ROLE.id,
-                  ROLE.role_name,
-                  ROLE.detail,
-                  ROLE.created_at,
-                  ROLE.updated_at,
-                  ROLE.permission,
-                  role_status.status_value AS status,
-                  (SELECT COUNT(*) AS numberOfRecords FROM user INNER JOIN role ON user.role = role.id WHERE user.role = ROLE.id ) AS totalUser 
-                  FROM role ROLE INNER JOIN role_status ON ROLE.status = role_status.id 
-                  ${whereClause} ${orderByClause} ${limitClause}`;
+                      ROLE.id,
+                      ROLE.role_name,
+                      ROLE.detail,
+                      ROLE.created_at,
+                      ROLE.updated_at,
+                      ROLE.permission,
+                      role_status.status_value AS status,
+                      (SELECT COUNT(*) AS numberOfRecords FROM user INNER JOIN role ON user.role = role.id WHERE user.role = ROLE.id ) AS totalUser 
+                FROM role ROLE INNER JOIN role_status ON ROLE.status = role_status.id 
+                ${whereClause} ${orderByClause} ${limitClause}`;
     connection.query(SQL, (error, result, field) => {
       if (error) return reject(error);
       resolve(result);
