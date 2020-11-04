a91ada1c0a211e8d01f2a0d51989ba6ed4e71ca1
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-04 11:23:33 +0700

    fix: edit indent in createNewRole

--- server/src/services/createNewRole.js
+++ server/src/services/createNewRole.js
@@ -3,12 +3,13 @@ const mysql = require("mysql");
 
 const createNewRole = async (role_name, detail, permission, status) => {
   return new Promise((resolve, reject) => {
-    const SQL = `INSERT INTO role(role_name, detail, permission, status) VALUES (
-                                ${mysql.escape(role_name)},
-                                ${mysql.escape(detail)},
-                                ${mysql.escape(JSON.stringify(permission))},
-                                (SELECT id FROM role_status WHERE status_value = ${status})
-                                )`;
+    const SQL = `INSERT INTO role(role_name, detail, permission, status) 
+                  VALUES (
+                          ${mysql.escape(role_name)},
+                          ${mysql.escape(detail)},
+                          ${mysql.escape(JSON.stringify(permission))},
+                          (SELECT id FROM role_status WHERE status_value = ${status})
+                        )`;
     connection.query(SQL, (error, result, field) => {
       if (error) return reject(error);
       resolve(result);
