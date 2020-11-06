5214e1b48bce72cc22c4a4f11dc615bc93035736
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-06 13:47:35 +0700

    add: createProductHandler model, createNewCurrentProductBalanceRecord service

---
+++ server/src/models/createProductHandler.js
@@ -0,0 +1,49 @@
+const {
+  createNewProduct,
+  createNewCurrentProductBalanceRecord,
+} = require("../services");
+const validateProductHandler = require("./validateProductHandler");
+
+const createProductHandler = async (req) => {
+  const response = {
+    success: false,
+  };
+  const {
+    product_id,
+    product_name,
+    company_name,
+    location,
+    detail,
+    status,
+  } = req.body;
+
+  req.query.validate = product_id;
+
+  const { success: isProductIdValid } = await validateProductHandler(req);
+  console.log(isProductIdValid);
+
+  if (!isProductIdValid) {
+    const createNewProductResult = await createNewProduct(
+      product_id,
+      product_name,
+      company_name,
+      location,
+      detail,
+      status,
+      req.decodedUsername
+    );
+
+    if (createNewProductResult) {
+      const createNewCurrentProductBalanceResult = await createNewCurrentProductBalanceRecord(
+        product_id,
+        location
+      );
+      if (createNewCurrentProductBalanceResult) {
+        response.success = true;
+      }
+    }
+  }
+  return response;
+};
+
+module.exports = createProductHandler;
--- server/src/services/index.js
+++ server/src/services/index.js
@@ -2,10 +2,12 @@ const getCurrentProductBalance = require("./getCurrentProductBalance");
 const getTotalNumberOfRecords = require("./getTotalNumberOfRecords");
 const getProduct = require("./getProduct");
 const createNewProduct = require("./createNewProduct");
+const createNewCurrentProductBalanceRecord = require("./createNewCurrentProductBalanceRecord");
 
 module.exports = {
   getCurrentProductBalance,
   getTotalNumberOfRecords,
   getProduct,
   createNewProduct,
+  createNewCurrentProductBalanceRecord,
 };
