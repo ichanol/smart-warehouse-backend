9bf89ea6f6b8f16403d913c7c30672807eb5dd2a
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-24 12:30:34 +0700

    fix: change variable name from product_serial_number => product_id in detectProductHandler

--- server/src/models/detectProductHandler.js
+++ server/src/models/detectProductHandler.js
@@ -1,13 +1,13 @@
 const { getProductInformationById } = require("../services");
 
 const detectProductHandler = async (data) => {
-  const productId = data.map((value) => value.product_serial_number);
+  const productId = data.map((value) => value.product_id);
 
   const productInformationResult = await getProductInformationById(productId);
 
   if (productInformationResult.length > 0) {
     const result = productInformationResult.map((value, key) => {
-      value.product_serial_number = data[key].product_serial_number;
+      value.product_id = data[key].product_id;
       value.amount = data[key].amount;
       return value
     });
