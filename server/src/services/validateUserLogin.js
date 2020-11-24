e29b637822a257f56d08024ab30a6d87745a8031
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-24 12:15:11 +0700

    fix: get user permission in validateUserLogin service

--- server/src/services/validateUserLogin.js
+++ server/src/services/validateUserLogin.js
@@ -3,9 +3,14 @@ const mysql = require("mysql");
 
 const validateUserLogin = (username, password) => {
   return new Promise((resolve, reject) => {
-    const SQL = `SELECT username FROM user WHERE username = ${mysql.escape(
-      username
-    )} AND password = ${mysql.escape(password)} AND status = 1`;
+    const SQL = `SELECT role_permission.permission, 
+                        role_permission.status 
+                  FROM user 
+                  INNER JOIN role_permission ON user.role = role_permission.role
+                  INNER JOIN user_status ON user.status = user_status.id
+                  WHERE user.username = ${mysql.escape(
+                    username
+                  )} AND user.password = ${mysql.escape(password)} AND user_status.status_value = 1`;
 
     connection.query(SQL, (error, result, field) => {
       if (error) return reject(error);
