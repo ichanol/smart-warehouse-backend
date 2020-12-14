fe8860d524f7e3c67e27993e3c8105e512b45a8a
Chanatip <chanatip.ras@gmail.kmutt.ac.th>
2020-12-14 17:44:45 +0700

    fix: sql syntax in getRoleId and createNewRole services

--- server/src/services/getRoleId.js
+++ server/src/services/getRoleId.js
@@ -1,8 +1,11 @@
 const connection = require("../Database_connection/connect");
+const mysql = require("mysql");
 
 const getRoleId = async (roleName) => {
   return new Promise((resolve, reject) => {
-    const SQL = `SELECT id FROM role WHERE role_name = ${roleName}`;
+    const SQL = `SELECT id FROM role WHERE role_name = ${mysql.escape(
+      roleName
+    )}`;
     connection.query(SQL, (error, result, field) => {
       if (error) return reject(error);
       const [{ id }] = result;
