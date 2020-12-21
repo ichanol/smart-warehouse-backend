454c84125a25deef8e65386367659de294de94e9
Chanatip <chanatip.ras@gmail.kmutt.ac.th>
2020-12-21 18:13:56 +0700

    fix: merge change

--- server/src/controllers/publicRouteControllers/index.js
+++ server/src/controllers/publicRouteControllers/index.js
@@ -2,6 +2,5 @@ const detectUserId = require("./detectUserId");
 const detectProductId = require("./detectProductId");
 const userLogin = require("./userLogin");
 const renewToken = require("./renewToken");
-const uploadFile = require("./uploadFile");
 
-module.exports = { detectUserId, detectProductId, userLogin, renewToken, uploadFile };
+module.exports = { detectUserId, detectProductId, userLogin, renewToken };
